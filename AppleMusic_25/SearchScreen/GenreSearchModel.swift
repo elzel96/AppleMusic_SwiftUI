d07b6ab3657048829235c3887b5ecfef4c3a2bc3
elzel <elzel666@gmail.com>
2024-02-05 13:24:53 +0300

    created_GenreModel

--- AppleMusic_25/SearchScreen/GenreSearchModel.swift
+++ AppleMusic_25/SearchScreen/GenreSearchModel.swift
@@ -1,8 +1,19 @@
-//
-//  GenreSearchModel.swift
-//  AppleMusic_25
-//
-//  Created by Helena on 5.02.2024.
-//
+import SwiftUI
 
-import Foundation
+struct GenreSearchModel: Hashable {
+    var imageName: String
+    var category: String
+}
+
+extension GenreSearchModel {
+    static var models: [GenreSearchModel] = [
+        GenreSearchModel(imageName: "01", category: "Tанцевальная"),
+        GenreSearchModel(imageName: "02", category: "K-pop"),
+        GenreSearchModel(imageName: "03", category: "Альтернативная"),
+        GenreSearchModel(imageName: "04", category: "Электроника"),
+        GenreSearchModel(imageName: "05", category: "Инди"),
+        GenreSearchModel(imageName: "06", category: "Хип-хоп на русском"),
+        GenreSearchModel(imageName: "07", category: "Хип-хоп"),
+        GenreSearchModel(imageName: "08", category: "Поп на русском")
+    ]
+}
