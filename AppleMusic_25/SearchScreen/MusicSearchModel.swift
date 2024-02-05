4a57fc3ca722ba5c4e2c4640bcc42dd2f79ec132
elzel <elzel666@gmail.com>
2024-02-05 13:26:41 +0300

    created_MusicSearchModel

--- AppleMusic_25/SearchScreen/MusicSearchModel.swift
+++ AppleMusic_25/SearchScreen/MusicSearchModel.swift
@@ -1,8 +1,20 @@
-//
-//  MusicSearchModel.swift
-//  AppleMusic_25
-//
-//  Created by Helena on 5.02.2024.
-//
+import SwiftUI
 
-import Foundation
+struct MusicSearchModel: Hashable {
+    var imageName: String
+    var title: String
+    var author: String
+    var type: String
+}
+
+extension MusicSearchModel {
+    static var models: [MusicSearchModel] = [
+        MusicSearchModel(imageName: "свободен", title: "Я свободен", author: "Кипелов", type: "Песня"),
+        MusicSearchModel(imageName: "психи", title: "Психи попадают в топ", author: "Макс Корж", type: "Альбом"),
+        MusicSearchModel(imageName: "герой", title: "Герой асфальта", author: "Ария", type: "Альбом"),
+        MusicSearchModel(imageName: "игра", title: "Игра с огнем", author: "Ария", type: "Альбом"),
+        MusicSearchModel(imageName: "воля", title: "Воля", author: "Макс Корж", type: "Песня"),
+        MusicSearchModel(imageName: "вомне", title: "Во мне", author: "yaskraviy", type: "Песня"),
+        MusicSearchModel(imageName: "мияги", title: "YAMAKASI", author: "Miyagi & Andy Panda", type: "Песня")
+    ]
+}
