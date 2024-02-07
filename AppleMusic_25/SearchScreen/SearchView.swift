92b7282df7823e9c808939952043426e36c61c4e
elzel <elzel666@gmail.com>
2024-02-07 03:32:09 +0300

    created_SearchView

--- AppleMusic_25/SearchScreen/SearchView.swift
+++ AppleMusic_25/SearchScreen/SearchView.swift
@@ -1,33 +1,57 @@
 import SwiftUI
 
+class TextFieldSettings: ObservableObject {
+    @Published var text = "Артисты, песни и др."
+}
+
 struct SearchView: View {
     private var models = GenreSearchModel.models
     private var columns: [GridItem] = Array(repeating: .init(.flexible(), spacing: 8), count: 2)
-    @State var searchTitle = "Поиск"
+    @State private var searchTitle = "Поиск"
+    @State private var searchText = ""
+    @StateObject var settings = TextFieldSettings()
+    @State private var isTextFieldActive = false
+    @State private var searchModel = MusicSearchModel.models
     
     var body: some View {
         NavigationView {
             VStack(alignment: .leading) {
-          //      TextField("First name", text: $user.userModel.firstName) // !!
+                NavigationLink(
+                    destination: ActiveSearchView().environmentObject(settings),
+                    isActive: $isTextFieldActive,
+                    label: {
+                        TextField("\(Image(systemName: "magnifyingglass"))  \(settings.text)", text: $searchText)
+                            .frame(height: 10)
+                            .multilineTextAlignment(.leading)
+                            .padding()
+                            .background(Color(UIColor.systemGray6))
+                            .foregroundColor(.gray)
+                            .cornerRadius(8)
+                            .onTapGesture {
+                                isTextFieldActive = true
+                            }
+                    }
+                )
+                
                 Text("Поиск по категориям")
-                    .frame(alignment: .leading)
-                    .font(.headline)
-                ScrollView(.vertical, showsIndicators: false) {
-                    LazyVGrid(columns: columns, spacing: 8) {
-                        ForEach(models, id: \.self) { model in
-                            NavigationLink {
-                                DetailsSearchView(title: model.category)
-                            } label: {
-                                Image(model.imageName)
-                                    .resizable()
-                                    .cornerRadius(6)
-                                    .frame(height: 115)
-                                    .cornerRadius(8)
-                                    .foregroundColor(.blue)
+                        .frame(alignment: .leading)
+                        .font(.headline)
+                    ScrollView(.vertical, showsIndicators: false) {
+                        LazyVGrid(columns: columns, spacing: 8) {
+                            ForEach(models, id: \.self) { model in
+                                NavigationLink {
+                                    DetailsSearchView(title: model.category)
+                                } label: {
+                                    Image(model.imageName)
+                                        .resizable()
+                                        .cornerRadius(6)
+                                        .frame(height: 115)
+                                        .cornerRadius(8)
+                                        .foregroundColor(.blue)
+                                }
                             }
                         }
                     }
-                }
             }
             .padding(.leading, 15)
             .padding(.trailing, 15)
@@ -38,11 +62,11 @@ struct SearchView: View {
         }
     }
 }
-    
-
 
 struct SearchView_Previews: PreviewProvider {
     static var previews: some View {
         SearchView()
     }
 }
+    
+    
